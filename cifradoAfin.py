e0964ac22f53ecd0a4493aabf6ea5f479c578cd5
milksake <caceresmariana443@gmail.com>
2022-04-28 17:41:06 -0500

    small changes

--- cifradoAfin.py
+++ cifradoAfin.py
@@ -3,6 +3,8 @@ from inverso import inverso
 ABC = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
 
 def cifrarAfin(mensaje, llave):
+    if not inverso(llave[0], 27):
+        return None
     newMensaje = ""
     for char in mensaje:
         newIndex = (llave[0] * ABC.find(char) + llave[1]) % 27
@@ -10,24 +12,28 @@ def cifrarAfin(mensaje, llave):
     return newMensaje
 
 def descifrarAfin(mensaje, llave):
-    newMensaje = ""
+    inv = inverso(llave[0], 27)
+    if not inv:
+        return None
+    newMensaje = ""    
     for char in mensaje:
-        inv = inverso(llave[0], 27)
-        if not inv:
-            return None
         newIndex = (inv * (ABC.find(char) - llave[1])) % 27
         newMensaje += ABC[newIndex]
     return newMensaje
 
 if __name__ == "__main__":
     llave = [4, 7]
+    print("Cifrado Afin de ELEMENTALMIQUERIDOWATSON:")
     print(cifrarAfin("ELEMENTALMIQUERIDOWATSON", llave))
+    print()
+    print("Descifrado Afin de OKHFSNKFNWFCWJHSNCHQYWFSWF:")
     print(descifrarAfin("OKHFSNKFNWFCWJHSNCHQYWFSWF", llave))
     print()
-
+    """ Para hallar la clave [23, 17] que decifra el mensaje se uso:
     for i in range(27):
         for j in range(27):
             print(descifrarAfin("SLBCMVRBSHZBTÑSRQVVMSZBVHÑBVRQVLALHZBTÑSRQVWQAXLZWÑAQFQV", [i, j]), i, j)
     print()
-
+    """
+    print("Descifrado Afin de SLBCMVRBSHZBTÑSRQVVMSZBVHÑBVRQVLALHZBTÑSRQVWQAXLZWÑAQFQV:")
     print(descifrarAfin("SLBCMVRBSHZBTÑSRQVVMSZBVHÑBVRQVLALHZBTÑSRQVWQAXLZWÑAQFQV", [23, 17]))
--- inverso.py
+++ inverso.py
@@ -2,8 +2,15 @@ from euclides import euclidesExt
 
 def inverso(a, n):
     (mcd, x, y) = euclidesExt(a, n)
-    if mcd == 1: return x % n
-    else: return None
+    if mcd == 1:
+        return x % n
+    return None
 
 if __name__ == "__main__":
-    print(inverso(-1, 7))
+    a = int(input("Ingrese a (entero): "))
+    n = int(input("Ingrese n (entero positivo): "))
+    inv = inverso(a, n)
+    if inv:
+        print(f"El inverso de a (mod n) es : {inv}")
+    else:
+        print("No se puede calcular el inverso\na no es entero, n no es entero positivo o a y n no son coprimos")
